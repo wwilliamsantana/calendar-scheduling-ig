0ac0827d4ed19927b9c9df8492c9adebe993a9c0
William <euwilliamsantana@gmail.com>
2024-09-02 22:06:58 -0300

    init: install design system, config eslint and pageExtension

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  pageExtensions: ['page.tsx', 'api.tsx', 'api.ts'],
+}
 
-export default nextConfig;
+export default nextConfig
