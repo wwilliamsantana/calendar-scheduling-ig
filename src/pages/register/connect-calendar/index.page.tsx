833376323b547457d4aff17895fcfeba3399129b
William <euwilliamsantana@gmail.com>
2024-09-18 15:25:42 -0300

    feat: Page Connect Calendar

---
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,34 @@
+import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
+import { Container, Header } from '../styles'
+import { ArrowRight } from 'phosphor-react'
+import { ConnectBox, ConnectItem } from './styles'
+
+export default function ConnectCalendar() {
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Text>
+          Conecte o seu calendário para verificar automaticamente as horas
+          ocupadas e os novos eventos à medida em que são agendados.
+        </Text>
+
+        <MultiStep currentStep={2} size={4} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Conectar
+            <ArrowRight />
+          </Button>
+        </ConnectItem>
+        <Button type="suubmit">
+          Proxímo passo
+          <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
