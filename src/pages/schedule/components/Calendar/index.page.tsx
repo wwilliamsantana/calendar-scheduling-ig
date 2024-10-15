d61c7afc0cdca090f7e7bfcfe0a482205121313f
William Santana <euwilliamsantana@gmail.com>
2024-10-15 07:20:33 -0300

    feat style: Calendar visual structure

---
+++ src/pages/schedule/components/Calendar/index.page.tsx
@@ -0,0 +1,61 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+
+export function Calendar() {
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Dezembro <span>2022</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            <th>DOM.</th>
+            <th>SEG.</th>
+            <th>TER.</th>
+            <th>QUA.</th>
+            <th>QUI.</th>
+            <th>SEX.</th>
+            <th>SAB.</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
