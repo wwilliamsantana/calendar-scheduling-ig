c58d65187094ccbf01be99a29095ed7ad5fd21e5
William <euwilliamsantana@gmail.com>
2024-11-09 08:11:05 -0300

    feat: API: Availability of schedules part 2

--- src/pages/api/users/[username]/availability.api.ts
+++ src/pages/api/users/[username]/availability.api.ts
@@ -57,5 +57,22 @@ export default async function handler(
     },
   )
 
-  return res.json({ possibleTimes })
+  const blockedTimes = await prisma.scheduling.findMany({
+    select: {
+      date: true
+    },
+    where: {
+      userId: user.id,
+      date: {
+        gte: referenceDate.set("hour", startHour).toDate(),
+        lte: referenceDate.set("hour", endHour).toDate(),
+      }
+    }
+  })
+
+  const availableTimes = possibleTimes.filter(time => {
+    return !blockedTimes.some(blockedTime => blockedTime.date.getHours() === time)
+  })
+  console.log(availableTimes)
+  return res.json({ possibleTimes, availableTimes })
 }
